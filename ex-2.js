37355f011f9f4474d27d9a93245393bc205327e7
Bura <bu_bangplee@hotmail.com>
2024-06-12 16:08:45 +0700

    feat: submit JavaScript Conditional Logic Assignment

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,9 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+if (lightBulbStatus === "Broken"){
+    console.log("Light bulb is Broken.");
+    lightBulbStatus = "Light bulb is Broken.";
+}else {
+    console.log(`Please choose the correct input (On/Off/Broken)`);
+}
--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,18 @@
 function calculateStudentGrade(score) {
   // Start coding here
+  if(score <= 100 && score >= 90){
+    return "A";
+  }else if((score <= 89 && score >= 80)){
+    return "B";
+  }else if((score <= 79 && score >= 70)){
+    return "C";
+  }else if((score <= 69 && score >= 60)){
+    return "D";
+  }else if((score <= 59 && score >= 0)){
+    return "F";
+  }else{
+    return "F";
+  }
 }
 
 // Example case
